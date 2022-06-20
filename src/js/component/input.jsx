204b7dd4fab46fc8bea3cc56891d139f14b146a7
Felipe Muñoz <felipe.munoz.1997@gmail.com>
2022-06-20 21:08:39 +0000

    avance

--- src/js/component/input.jsx
+++ src/js/component/input.jsx
@@ -1,10 +1,47 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Input = () => {
 	const [inputValue, setinputValue] = useState("");
 	const [Task, setTask] = useState([]);
-	const deleteTask = (currentTask) => {};
+    const [Tarea, setTarea] = useState("");
+    const List = [{label:Tarea,done:false}]
+    useEffect(()=>{
+        getTask ()
+    }, [])
+    function getTask () {
+
+        var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+};
+
+        fetch("https://assets.breatheco.de/apis/fake/todos/user/felipin", requestOptions)
+        .then(response => response.json())
+        .then(result => {console.log(result),
+        setTarea(result);
+        })
+        .catch(error => console.log('error', error));
+    } 
+    function fetchPut (List){
+        var raw = JSON.stringify(List);
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        var requestOptions = {
+            method: 'PUT',
+            header: myHeaders,
+            body: raw,
+            redirect: 'follow'
+          };
+          
+          fetch("https://assets.breatheco.de/apis/fake/todos/user/felipin", requestOptions)
+            .then(response => response.json())
+            .then(result => console.log(result))
+            .catch(error => console.log('error', error));
+    }
+        
+        
 	console.log(inputValue, Task);
 	return (
 		<div className="contenedor">
@@ -21,9 +58,16 @@ const Input = () => {
 							if (e.key === "Enter") {
 								setTask(Task.concat(inputValue));
 								setinputValue(" ");
+                                fetchPut(List);
 							}
 						}}
 					/>
+                    <button
+                    onClick={() => {
+                        fetchPut(List);
+                    }}>
+                    Agregar tarea
+                </button>
 				</li>
 				{Task.map((value, index) => {
 					return (
